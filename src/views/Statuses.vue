6122930fca00dd1ec97a8a303c4b5158c4d9feda
lukachi <lukachisama@gmail.com>
2020-12-16 19:41:23 +0200

    statuses

--- src/views/Statuses.vue
+++ src/views/Statuses.vue
@@ -1,16 +1,72 @@
 <template>
   <div class="statuses">
-    <statuses-table class="statuses__table" />
+    <template v-if="isLoaded">
+      <template v-if="isLoadFailed">
+        <b-alert show variant="danger">
+          Error on load, please try to reload page
+        </b-alert>
+      </template>
+      <template v-else>
+        <template v-if="statuses.length">
+          <statuses-table class="statuses__table" />
+        </template>
+        <template v-else>
+
+        </template>
+      </template>
+    </template>
+    <template v-else>
+      <div>
+        <b-skeleton-table
+          :rows="5"
+          :columns="4"
+          :table-props="{ bordered: true, striped: true }"
+        ></b-skeleton-table>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import StatusesTable from '@/views/Statuses/StatusesTable'
+import {api} from "@/api";
+import {vuexTypes} from "@/vuex";
+import {mapGetters} from "vuex";
 export default {
   name: "statuses",
   components: {
     StatusesTable
-  }
+  },
+  data: _ => ({
+    isLoaded: false,
+    isLoadFailed: false,
+    statuses: [],
+  }),
+  computed: {
+    ...mapGetters([
+      vuexTypes.account
+    ])
+  },
+  async created () {
+    await this.loadStatuses()
+  },
+  methods: {
+    async loadStatuses () {
+      this.isLoaded = false
+      this.isLoadFailed = false
+      try {
+        await api.get('/services/getUserServices', {
+          params: {
+            email: this.account.email,
+          }
+        })
+      } catch (error) {
+        this.isLoadFailed = true
+        console.log(error)
+      }
+      this.isLoaded = true
+    }
+  },
 }
 </script>
 
