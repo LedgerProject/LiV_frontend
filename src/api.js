3964a00cf2c91ab64cf6afa3339b39b39167f308
Alexey Shapoval <as@eubclab.com>
2021-02-03 12:41:36 +0200

    Update base URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 export const api = Axios.create({
-  baseURL: 'http://ec2-18-192-22-193.eu-central-1.compute.amazonaws.com:8081',
+  baseURL: 'https://platform.livpoc.com:8081',
 })
 
 api.generateKeyPair = (secret) => {
